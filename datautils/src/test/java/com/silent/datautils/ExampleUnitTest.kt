8077da2515f7b1dbaf1398be969f84c07bbe36e4
wanglei <wanglei3@autoai.com>
2022-05-25 17:18:18 +0800

    add encrypt decrypt

--- datautils/src/test/java/com/silent/datautils/ExampleUnitTest.kt
+++ datautils/src/test/java/com/silent/datautils/ExampleUnitTest.kt
@@ -2,16 +2,15 @@ package com.silent.datautils
 
 import org.junit.Test
 
-import org.junit.Assert.*
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun encrypt() {
+        println(DataUtils.toHexString(byteArrayOf(1, 2, 3, 4, 5, 6, 7)))
     }
 }
